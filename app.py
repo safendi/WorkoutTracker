9a1df79f59d2d94ede1d26a0a214327399c76995
safendi <158005894+safendi@users.noreply.github.com>
2024-06-25 16:50:32 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,174 @@
+from flask import Flask, render_template, request, redirect, session
+import sqlite3
+import json
+
+app = Flask(__name__)
+
+app.secret_key = "sheffg"
+#app.permanent_session_lifetime = True
+
+conn = sqlite3.connect("users.db", check_same_thread=False)
+cursor = conn.cursor()
+cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+                    username text PRIMARY KEY,
+                    password text)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS workouts (
+                    username text PRIMARY KEY,
+                    workouts text)""")
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+        keypass = cursor.fetchone()
+        if keypass is not None:
+            print(keypass)
+            correctPass = keypass[1]
+            if password == correctPass:
+                session['usr'] = username
+                return redirect("/home")
+            else:
+                print("wrong password")
+                return redirect("/")
+        else:
+            print("User not found")
+            return redirect("/")
+
+    else:
+        if 'usr' in session:
+            return redirect("/home")
+        else :
+            return render_template('index.html')
+
+
+
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+        keypass = cursor.fetchone()
+        if keypass is None:
+            cursor.execute("INSERT INTO users VALUES (?, ?)", (username, password))
+            conn.commit()
+            session['usr'] = username
+            return redirect("/home")
+        else:
+            print("User already exists")
+            return redirect("/register")
+
+    else:
+        return render_template('register.html')
+
+
+
+
+@app.route('/home')
+def home():
+    if 'usr' in session:
+        return render_template('home.html', usr=session['usr'])
+    else :
+        return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('usr', None)
+    return redirect("/")
+
+
+
+@app.route('/list', methods=['GET', 'POST'])
+def data():
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        for key in data:
+            dataKey = key
+            data = data[dataKey]
+        print(data)
+        cursor.execute("SELECT * FROM workouts WHERE username = ?", (session['usr'],))
+        list = cursor.fetchone()
+        if (list is None):
+            newList = {}
+            newList[dataKey] = data
+            newList = json.dumps(newList)
+            cursor.execute("INSERT INTO workouts VALUES (?, ?)", (session['usr'], newList))
+            conn.commit()
+        else:
+            list = list[1]
+            list = json.loads(list)
+            list[dataKey] = data
+            list = json.dumps(list)
+            cursor.execute("REPLACE INTO workouts VALUES (?, ?)", (session['usr'], list))
+            conn.commit()
+        list = cursor.execute("SELECT * FROM workouts WHERE username = ?", (session['usr'],))
+        result = list.fetchone()
+        print(result[1])
+        return result[1]
+
+    elif request.method == 'GET':
+        list = cursor.execute("SELECT * FROM workouts WHERE username = ?", (session['usr'],))
+        result = list.fetchone()
+        if result == None:
+            return "None"
+        else:
+            result = result[1]
+            data = json.loads(result)
+            return json.dumps(data)
+
+
+@app.route('/removeWo', methods=['POST'])
+def removeWo():
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        get = cursor.execute("SELECT * FROM workouts WHERE username = ?", (session['usr'],))
+        list = get.fetchone()
+        list = json.loads(list[1])
+        print(list)
+        list.pop(data)
+        print(list)
+        list = json.dumps(list)
+        cursor.execute("REPLACE INTO workouts VALUES (?, ?)", (session['usr'], list,))
+        conn.commit()
+        return list
+
+
+@app.route('/workoutSelected', methods=['POST', 'GET'])
+def workoutSelected():
+    if request.method == 'POST':
+        data = request.get_json()
+        session['currentWorkout'] = data
+        print(session['currentWorkout'])
+        return session['currentWorkout']
+    if request.method == 'GET':
+        if 'currentWorkout' in session:
+            currentWorkout = session['currentWorkout']
+            get = cursor.execute("SELECT * FROM workouts WHERE username = ?", (session['usr'],))
+            list = get.fetchone()
+            list = json.loads(list[1])
+            data = list[currentWorkout]
+            retData = {}
+            retData[currentWorkout] = data
+            return json.dumps(retData)
+        else:
+            return "None"
+
+
+
+@app.route('/workout')
+def workout():
+    return render_template('workout.html')
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/home.js
@@ -0,0 +1,236 @@
+
+function myWorkoutsClicked() {
+
+    $.ajax({
+                url: '/list',
+                type: 'GET',
+                contentType: 'application/json',
+                success: function(response) {
+                    if (response == "None") {
+                        console.log("none found")
+                    } else {
+                        res = JSON.parse(response)
+                        console.log(res)
+                        loadWorkouts(res)
+                    }
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+}
+
+function loadWorkouts(arg) {
+    document.getElementById("deleteConfirmation").hidden = true
+    let currentWorkoutList = document.getElementById("currentWorkoutsListTable")
+    while (currentWorkoutList.rows.length != 0) {
+        currentWorkoutList.deleteRow(0)
+    }
+    document.getElementById("workoutCreator").hidden = true
+    document.getElementById("workoutNamer").hidden = true
+    document.getElementById("currentWorkoutsList").hidden = false
+    if (Object.keys(arg).length == 0) {
+        document.getElementById("noWorkoutsCurrently").hidden = false
+        document.getElementById("currentWorkoutsListTable").hidden = true
+    } else {
+        document.getElementById("noWorkoutsCurrently").hidden = true
+        document.getElementById("currentWorkoutsListTable").hidden = false
+        for (let key in arg) {
+            console.log(key)
+            let row = currentWorkoutList.insertRow(0)
+            let cell1 = row.insertCell(0)
+            let cell2 = row.insertCell(1)
+            let button = document.createElement("button")
+            let button2 = document.createElement("button")
+            button.textContent = "Delete"
+            button2.textContent = "Edit"
+            cell1.innerHTML = key
+            cell2.appendChild(button2)
+            cell2.appendChild(button)
+            button.onclick = function () {
+                //are you sure screen, if yes:
+                document.getElementById("deleteConfirmation").hidden = false
+                document.getElementById("deleteConfirmButton").onclick = function () {
+                    document.getElementById("deleteConfirmation").hidden = true
+                    removeWorkout(key)
+                    for (let i = 0; i < currentWorkoutList.rows.length; i++) {
+                        let row = currentWorkoutList.rows[i]
+                        let ex = row.cells[0].innerHTML
+                        if (ex === key) {
+                            currentWorkoutList.deleteRow(i)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+function removeWorkout(key) {
+    $.ajax({
+                url: '/removeWo',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(key),
+                success: function(response) {
+                    console.log(response)
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+}
+
+function updateList(list) {
+    $.ajax({
+                url: '/list',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(list),
+                success: function(response) {
+                    console.log(response)
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+}
+
+
+let workoutList = {}
+let woName = ""
+let exList = []
+
+function unhideWoNamer() {
+    let namer = document.getElementById("workoutNamer")
+    namer.hidden = false
+    document.getElementById("currentWorkoutsList").hidden = true
+}
+
+
+function setWoName() {
+    woName = document.getElementById("woName").value
+    if (woName.length > 0) {
+        document.getElementById("woNameLabel").innerHTML = woName
+        document.getElementById("notEnoughCharinWoNameWarning").hidden = true
+        document.getElementById("workoutNamer").hidden = true
+        document.getElementById("workoutCreator").hidden = false
+    } else {
+        document.getElementById("notEnoughCharinWoNameWarning").hidden = false
+    }
+}
+
+function addEx() {
+    document.getElementById("noExercises").hidden = true
+    let woTable = document.getElementById("woTable")
+    let exercise = document.getElementById("exName").value
+    let sets = document.getElementById("setsNum").value
+    if (exList.includes(exercise)) {
+        while(exList.includes(exercise)) {
+            exercise = exercise + "*"
+        }
+    }
+    let row = woTable.insertRow(exList.length + 1)
+    let cell1 = row.insertCell(0)
+    let cell2 = row.insertCell(1)
+    cell1.innerHTML = exercise
+    cell2.innerHTML = sets
+    let val = []
+    val.push(sets)
+    for (let i = 0; i < sets*2; i++) {
+        val.push(0)
+    }
+    workoutList[exercise] = val
+    exList.unshift(exercise)
+}
+
+function undoAddEx() {
+    let woTable = document.getElementById("woTable")
+    if (exList.length != 0) {
+        woTable.deleteRow(exList.length)
+    } else {
+        console.log("Must have at least 1 exercise")
+    }
+    delete workoutList[exList[0]]
+    exList.shift()
+}
+
+function completeWoCreation() {
+    if (Object.keys(workoutList).length === 0) {
+        document.getElementById("noExercises").hidden = false
+    } else {
+        document.getElementById("noExercises").hidden = true
+        data = {}
+        data[woName] = workoutList
+        updateList(data)
+        workoutList = {}
+        exList = []
+        woName = ""
+    }
+    document.getElementById("workoutCreator").hidden = true
+    document.getElementById("currentWorkoutsListTable").hidden = true
+    let currentWorkoutList = document.getElementById("woTable")
+    while (currentWorkoutList.rows.length != 1) {
+        currentWorkoutList.deleteRow(1)
+    }
+}
+
+function pageLoaded() {
+
+    $.ajax({
+                url: '/list',
+                type: 'GET',
+                contentType: 'application/json',
+                success: function(response) {
+                    if (response == "None") {
+                        console.log("none found")
+                    } else {
+                        res = JSON.parse(response)
+                        loadCurrentWorkouts(res)
+                    }
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+}
+
+function loadCurrentWorkouts(arg) {
+    let box = document.getElementById("currentWorkouts")
+    box.innerHTML = ""
+    for (let key in arg) {
+        let div = document.createElement("div")
+        let name = document.createElement("h3")
+        name.innerHTML = key
+        div.appendChild(name)
+        let listOfEx = document.createElement("p")
+        for (let ex of Object.keys(arg[key])) {
+            listOfEx.innerHTML += ex + " (x" + arg[key][ex][0] + ")" + ", "
+        }
+        listOfEx.innerHTML = listOfEx.innerHTML.substring(0, listOfEx.innerHTML.length - 2)
+        listOfEx.style.fontStyle = "italic"
+        listOfEx.className = "listOfEx"
+        div.appendChild(listOfEx)
+        let button = document.createElement("button")
+        button.innerHTML = "Start"
+        button.onclick = function() {
+            $.ajax({
+                url: '/workoutSelected',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(key),
+                success: function(response) {
+                    console.log(response)
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+            window.location.href = '/workout'
+        }
+        div.appendChild(button)
+        box.appendChild(div)
+    }
+}
+
+
